0d4c16212eb081b9d91a05ddb0827bb549af27a1
MohitAlphonsus <mohitdalphonso@gmail.com>
2024-04-30 19:29:26 +0530

    refactoring styles

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,4 +1,5 @@
 import project1 from '../assets/project-1.png';
+import project2 from '../assets/project-2.png';
 
 export const projects = [
 	{
@@ -11,6 +12,19 @@ export const projects = [
 			{ id: 't2', toolName: 'Styled-Components' },
 			{ id: 't3', toolName: 'React-Icons' },
 			{ id: 't4', toolName: 'Swiper' },
+		],
+	},
+	{
+		id: 'p2',
+		imgSrc: project2,
+		projectName: 'Furninest',
+		desc: 'Furniture store with browser storage and cart functionality',
+		tools: [
+			{ id: 't1', toolName: 'React' },
+			{ id: 't2', toolName: 'CSS-Modules' },
+			{ id: 't3', toolName: 'React-Icons' },
+			{ id: 't4', toolName: 'React-Router-Dom' },
+			{ id: 't4', toolName: 'Browser-Storage' },
 		],
 	},
 ];
